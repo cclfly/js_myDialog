14ae54e08d61a1b4b7614a1b7b962ca77270d09c
cclfly <1171407627@qq.com>
2016-09-02 13:37:31 +0800

    完善
    完善dialog的自动居中，增加关闭方法

--- js/dialog.class.js
+++ js/dialog.class.js
@@ -1,72 +1,85 @@
+function myAddEvent(obj,sEv,fn)
+{
+	if(obj.attachEvent)
+	{
+		obj.attachEvent("on"+sEv,fn);
+	}
+	else
+	{
+		obj.addEventListener(sEv,fn,false);
+	}
+}
+
+function myDelEvent(obj,sEv,fn)
+{
+	if(obj.detachEvent)
+	{
+		obj.detachEvent("on"+sEv,fn);
+	}
+	else
+	{
+		obj.removeEventListener(sEv,fn,false);
+	}
+}
+
 function Dialog(width,height){
+	this.obj = null;
 	this.Init(width||320,height||60);
-	this.flagAutoMove = true;
-	this.OpenAutoMove();
+	this.fnInit = this.Init.bind(this);				//自动居中用来绑定的函数
+	this.OpenAutoCenter();
 }
 
 Dialog.prototype = {
 	Init:function(width,height){
-		this.width=width;
-		this.height=height;
-		this.left=((window.innerWidth||(document.body&&document.body.clientWidth))-this.width)/2;
-		this.top=((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/2-((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/20;
-		
 		if(!this.obj)
 		{
+			this.width = width;
+			this.height = height;
 			this.obj = document.createElement('div');
-			this.obj.style.position="absolute";
-			this.obj.style.backgroundColor="#66ccff";
-			this.obj.style.display="none";
-			document.documentElement.appendChild(this.obj);
+			this.obj.style.backgroundColor = "#66ccff";			//测试方便
+			this.obj.style.position = "absolute";
+			this.obj.style.display = "none";
+			(document.body||document.documentElement).appendChild(this.obj);
 		}
-		this.obj.style.width=this.width+"px";
-		this.obj.style.height=this.height+"px";
-		this.obj.style.left=this.left+"px";
-		this.obj.style.top=this.top+"px";
+		this.left = ((window.innerWidth||(document.body&&document.body.clientWidth))-this.width)/2;
+		this.top = ((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/2-((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/20;
+		
+		this.obj.style.width = this.width+"px";
+		this.obj.style.height = this.height+"px";
+		this.obj.style.left = this.left+"px";
+		this.obj.style.top = this.top+"px";
 	},
 	Resize:function(width,height){
-		this.width=width;
-		this.height=height;
-		this.obj.style.width=this.width+"px";
-		this.obj.style.height=this.height+"px";
+		this.width = width;
+		this.height = height;
+		this.obj.style.width = this.width+"px";
+		this.obj.style.height = this.height+"px";
 	},
 	Show:function(diaText,diaTitle){
 		var text = diaText||"";
 		var title = diaTitle||"dialog";
-		this.obj.style.display="block";
+		this.obj.style.display = "block";
 	},
-	Close:function(){},
-	CloseAutoMove(){
-		this.flagAutoMove=false;
+	Close:function(){
+		(document.body||document.documentElement).removeChild(this.obj);
 	},
-	OpenAutoMove(){
-		var w=(window.innerWidth||(document.body&&document.body.clientWidth));
-		var h=(window.innerHeight||(document.body&&document.body.clientHeight));
+	CloseAutoCenter:function(){
+		myDelEvent(window,"resize",this.fnInit);
+	},
+	OpenAutoCenter:function(){
+		myAddEvent(window,"resize",this.fnInit);
+	},
+	Move:function(left,top,width,height){
+		this.CloseAutoCenter();
 		
-		this.timerAutoMove = setInterval(function(){
-			if((window.innerWidth||(document.body&&document.body.clientWidth))!=w||(window.innerHeight||(document.body&&document.body.clientHeight))!=h)
-			{
-				this.left=((window.innerWidth||(document.body&&document.body.clientWidth))-this.width)/2;
-				this.top=((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/2-((window.innerHeight||(document.body&&document.body.clientHeight))-this.height)/20;
-				this.obj.style.left=this.left+"px";
-				this.obj.style.top=this.top+"px";
-				w=(window.innerWidth||(document.body&&document.body.clientWidth));
-				h=(window.innerHeight||(document.body&&document.body.clientHeight));
-			}
-			if(!this.flagAutoMove&&this.timerAutoMove)
-			{
-				clearInterval(this.timerAutoMove);
-			}
-		}.bind(this),30);
-	},
-	Move(left,top){
-		if(this.flagAutoMove)
-		{
-			this.CloseAutoMove();
-		}
-		this.left=left;
-		this.top=top;
-		this.obj.style.left=this.left+"px";
-		this.obj.style.top=this.top+"px";
+		this.left = left||this.left;
+		this.top = top||this.top;
+		this.width = width||this.width;
+		this.height = height||this.height;
+		
+		this.obj.style.left = this.left + "px";
+		this.obj.style.top = this.top + "px";
+		this.obj.style.width = this.width + "px";
+		this.obj.style.height = this.height + "px";
 	}
 };
